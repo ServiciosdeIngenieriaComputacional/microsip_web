eb56aede7db705fd7bb8e3a59d3b6390d4dc6e72
Jesús Herrera <jesusmaherrera@gmail.com>
2013-04-08 15:11:30 -0600

    solo cp

--- microsip_web/urls.py
+++ microsip_web/urls.py
@@ -16,13 +16,18 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
 	(r'^$', 'inventarios.views.index'),
-    url(r'^inventarios/', include('inventarios.urls', namespace='Inventarios')),
-    url(r'^ventas/', include('ventas.urls', namespace='ventas')),
+    #Descomentar esta linea para habilitar inventarios
+    #url(r'^inventarios/', include('inventarios.urls', namespace='Inventarios')),
+    url(r'^inventarios/', 'inventarios.views.index'),
+    #Descomentar esta linea para habilitar ventas
+    #url(r'^ventas/', include('ventas.urls', namespace='ventas')),
+    url(r'^ventas/', 'inventarios.views.index'),
+    
     url(r'^cuentas_por_pagar/', include('cuentas_por_pagar.urls', namespace='cuentas_por_pagar')),
     url(r'^contabilidad/', include('contabilidad.urls', namespace='contabilidad')),
     
     url(r'autocomplete/', include('autocomplete_light.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
     #LOGIN
     url(r'^login/$','inventarios.views.ingresar'),
     url(r'^logout/$', 'inventarios.views.logoutUser'),
